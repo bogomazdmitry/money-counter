1708786b410089cfedd289a375ea15a6d00a8e56
Bogomaz Dmitry <bogomaz.dima2013@gmail.com>
2024-12-08 19:24:33 +0100

    upgrade count of balances

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-from state import get_balance, update_balance
+from state import change_limit_for_type, get_balance_info, reset_limits_for_chat, spend_balance_for_type, upsert_balance_type
 
 # Enable logging
 logging.basicConfig(
@@ -47,55 +47,110 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         "Available commands:\n"
         "/start - Welcome message and basic information.\n"
         "/help - Show this help message.\n"
-        "/set_current_balance <amount> - Set the current balance to the specified amount.\n"
-        "/get_current_balance - Get the current balance.\n"
+        "/get_all_balance_info - Get full info about balance.\n"
+        "/upsert_balance <limit> <type> - Set the current balance to the specified limit.\n"
+        "/change_limit <limit> <type> - Change limit for balance.\n"
+        "/reset_limits - Reset all balances.\n"
         "Simply send a message with a number to count that amount and update the balance."
     )
     await update.message.reply_text(help_text)
 
-async def count(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Count money."""
+# Send all balance info to chat with formatted version, like as <Type>: <balance>/<limit>
+async def get_all_balance_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Get all balance info."""
+    logging.info(f"Get all balance info for chat {update.message.chat_id}")
+    chat_id = update.message.chat_id
+    balance_info = await get_balance_info(context, chat_id)
+    if balance_info:
+        result_string = "Balance info:\n"
+        for type, info in balance_info.items():
+            result_string += f"{type}: {info['balance']}/{info['limit']}\n"
+        await update.message.reply_text(result_string)
+    else:
+        await update.message.reply_text("No balances found.")
+
+async def upsert_balance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Upsert balance."""
+    chat_id = update.message.chat_id
+    logging.info(f"Upsert balance for chat {chat_id}")
+    args = update.message.text.strip().split()[1:]
+    if len(args) != 2:
+        await update.message.reply_text("Please send two arguments: type and limit.")
+        return
+    limit, type = args
+    try:
+        limit = float(limit)
+    except ValueError:
+        await update.message.reply_text("Limit must be a number.")
+        return
+    await upsert_balance_type(context, chat_id, type, limit)
+    await update.message.reply_text(f"Balance for {type} set to {limit}.")
+
+async def change_limit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Change limit."""
+    logging.info(f"Change limit for chat {update.message.chat_id}")
+    chat_id = update.message.chat_id
+    args = update.message.text.strip().split()[1:]
+    if len(args) != 2:
+        await update.message.reply_text("Please send two arguments: type and limit.")
+        return
+    limit, type = args
+    try:
+        limit = float(limit)
+    except ValueError:
+        await update.message.reply_text("Limit must be a number.")
+        return
+    result = await change_limit_for_type(context, chat_id, type, limit)
+    if result:
+        await update.message.reply_text(f"Limit for {type} changed to {limit}.")
+    else:
+        await update.message.reply_text(f"No balance found for {type}.")
+
+async def reset_limits(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Reset limits."""
+    logging.info(f"Reset limits for chat {update.message.chat_id}")
+    chat_id = update.message.chat_id
+    result = await reset_limits_for_chat(context, chat_id)
+    if result:
+        if 'error' in result:
+            await update.message.reply_text(result['error'])
+            return
+        result_string = "Old balances:\n"
+        for type, info in result['old'].items():
+            result_string += f"{type}: {info['balance']}/{info['limit']}\n"
+        result_string += "\nBalances reset."
+    else:
+        result_string = "No balances found."
+    await update.message.reply_text(result_string)
+
+async def spend(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Spend money."""
+    logging.info(f"Count money for chat {update.message.chat_id}")
     chat_id = update.message.chat_id
 
     try:
-        message_text = update.message.text.strip().split()[0]
-        logging.info(f"Count money with {message_text}")
-        amount = float(message_text)
-        current_balance = await get_balance(context, chat_id)
-        new_balance = current_balance - amount
-        await update_balance(context, chat_id, new_balance)
-        await update.message.reply_text(f"Counted {amount}. Current balance is {new_balance}.")
+        parts = update.message.text.strip().split()
+        amount_message_text = parts[0]
+        type = parts[1]
+        logging.info(f"Spend money with {amount_message_text}")
+        amount = float(amount_message_text)
+        new_balance = await spend_balance_for_type(context, chat_id, type, amount)
+        if new_balance is None:
+            await update.message.reply_text(f"No balance found for {type}.")
+        else:
+            await update.message.reply_text(f"Spent {amount} for type {type}. Current balance is {new_balance}.")
     except ValueError:
         await update.message.reply_text("Please send a valid number.")
 
-async def set_current_balance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Set current balance."""
-    chat_id = update.message.chat_id
-
-    message_text = update.message.text.split(' ', 1)
-    if len(message_text) == 2:
-        try:
-            balance = float(message_text[1])
-            await update_balance(context, chat_id, balance)
-            await update.message.reply_text(f"Current balance set to {balance}.")
-        except ValueError:
-            await update.message.reply_text("Please send a valid number.")
-    else:
-        await update.message.reply_text("Usage: /set_current_balance <amount>")
-
-async def get_current_balance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Get current balance."""
-    chat_id = update.message.chat_id
-    balance = await get_balance(context, chat_id)
-    await update.message.reply_text(f"Current balance is {balance}.")
-
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("help", help_command))
-app.add_handler(CommandHandler("set_current_balance", set_current_balance))
-app.add_handler(CommandHandler("get_current_balance", get_current_balance))
+app.add_handler(CommandHandler("get_all_balance_info", get_all_balance_info))
+app.add_handler(CommandHandler("upsert_balance", upsert_balance))
+app.add_handler(CommandHandler("change_limit", change_limit))
+app.add_handler(CommandHandler("reset_limits", reset_limits))
 
 # on non command i.e message - echo the message on Telegram
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, count))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, spend))
 
 if WEB_HOOK_HOST:
     app.bot.set_webhook(WEB_HOOK_HOST, allowed_updates=Update.ALL_TYPES)
--- state.py
+++ state.py
@@ -2,23 +2,21 @@ import json
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
 
-# Function to get current balance from pinned message
-async def get_balance(context: ContextTypes.DEFAULT_TYPE, chat_id: int) -> float:
+# Function to get data from pinned messages
+async def _get_data_from_pinned_messages(context: ContextTypes.DEFAULT_TYPE, chat_id: int) -> float:
     chat = await context.bot.get_chat(chat_id)
     pinned_message = chat.pinned_message
     if pinned_message and "Data for money-counter" in pinned_message.text:
         try:
-            data = json.loads(pinned_message.text.split('\n', 1)[1])
-            return data.get('balance', 0.0)
+            return json.loads(pinned_message.text.split('\n', 1)[1])
         except (IndexError, ValueError, json.JSONDecodeError):
             pass
-    return 0.0
+    return None
 
-# Function to update pinned message with current balance
-async def update_balance(context: ContextTypes.DEFAULT_TYPE, chat_id: int, balance: float):
+# Function to update pinned message with data
+async def _update_data_from_pinned_messages(context: ContextTypes.DEFAULT_TYPE, chat_id: int, data: object):
     chat = await context.bot.get_chat(chat_id)
     pinned_message = chat.pinned_message
-    data = {'balance': balance}
     message_text = f"Data for money-counter\n{json.dumps(data)}"
 
     if pinned_message and "Data for money-counter" in pinned_message.text:
@@ -26,3 +24,65 @@ async def update_balance(context: ContextTypes.DEFAULT_TYPE, chat_id: int, balan
     else:
         sent_message = await context.bot.send_message(chat_id, message_text, parse_mode=ParseMode.HTML)
         await context.bot.pin_chat_message(chat_id, sent_message.message_id)
+
+# Function to get current balance from pinned message per type
+async def get_balance_info_by_type(context: ContextTypes.DEFAULT_TYPE, chat_id: int, type: str) -> float:
+    data = await _get_data_from_pinned_messages(context, chat_id)
+    if data is None:
+        return None
+    if type not in data:
+        return None
+    return data[type]
+
+# Function to get full info about balance from pinned message
+async def get_balance_info(context: ContextTypes.DEFAULT_TYPE, chat_id: int) -> object:
+    return await _get_data_from_pinned_messages(context, chat_id)
+
+# Function to upsert balance type info with some limit
+async def upsert_balance_type(context: ContextTypes.DEFAULT_TYPE, chat_id: int, type: str, limit: float):
+    data = await _get_data_from_pinned_messages(context, chat_id)
+    if data is None:
+        data = {}
+    data[type] = {'limit': limit, 'balance': limit}
+    await  _update_data_from_pinned_messages(context, chat_id, data)
+
+# Function to change limit for type. Returns True if limit was changed, False otherwise
+async def change_limit_for_type(context: ContextTypes.DEFAULT_TYPE, chat_id: int, type: str, limit: float) -> bool:
+    data = await  _get_data_from_pinned_messages(context, chat_id)
+    if data is None:
+        return False
+    if type not in data:
+        return False
+    data[type]['limit'] = limit
+    await _update_data_from_pinned_messages(context, chat_id, data)
+    return True
+
+# Function to change balance for type. Returns True if balance was changed, False otherwise
+async def spend_balance_for_type(context: ContextTypes.DEFAULT_TYPE, chat_id: int, type: str, spent_balance: float) -> float:
+    data = await _get_data_from_pinned_messages(context, chat_id)
+    if data is None:
+        return None
+    if type not in data:
+        return None
+    new_balance = data[type]['balance'] - spent_balance
+    data[type]['balance'] = new_balance
+    await _update_data_from_pinned_messages(context, chat_id, data)
+    return new_balance
+
+
+# Function to reset all balances. Return old and new data
+async def reset_limits_for_chat(context: ContextTypes.DEFAULT_TYPE, chat_id: int) -> object:
+    data = await _get_data_from_pinned_messages(context, chat_id)
+    if data is None:
+        return None
+    old_data = data.copy()
+    have_changes = False
+    for type in data:
+        if data[type]['balance'] != data[type]['limit']:
+            have_changes = True
+        data[type]['balance'] = data[type]['limit']
+    if have_changes:
+        await _update_data_from_pinned_messages(context, chat_id, data)
+        return {'old': old_data, 'new': data}
+    else:
+        return {'error': 'No changes.'}
